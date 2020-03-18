5103457d0591c4b926161ac6025eaf61b1b78da8
Justin Liu <justin.xin.liu@gmail.com>
2020-03-18 12:10:12 -0700

    Responsive layout done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const cards = [];
+
+  for (let i = 0; i < 26; i++) {
+    const card = {
+      id: i.toString(),
+      title: "My card",
+      summary:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Nullam dictum felis eu pede mollis pretium.",
+      lastUpdated: new Date()
+    };
+
+    cards.push(card);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header role="banner">
+        <h3>
+          <mark>Dual-screen aware</mark> layout
+        </h3>
       </header>
-    </div>
+
+      <main role="main">
+        <section className="card-list">
+          {cards.map((card, i) => (
+            <article className="card" key={i}>
+              <span>Dec 12</span>
+
+              <h2>{card.title}</h2>
+
+              <hr />
+              <p>{card.summary}</p>
+            </article>
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
 
